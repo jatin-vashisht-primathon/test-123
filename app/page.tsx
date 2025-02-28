e320abc51d3cd06036c438b3f01b2d4497190dfd
jatin-vashisht-primathon <jatin.vashisht@primathon.in>
2025-02-28 16:03:47 +0530

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function App() {
         </div>
       )}
       <p className="bg-green-500">Hello There Everyone</p>
-      <Image src='https://plus.unsplash.com/premium_photo-1738105946749-320f638ed0be?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Image' height="300" width="300" />
+      <Image src='/test.jpg' alt='Image' height="300" width="300" />
     </>
   );
 }
