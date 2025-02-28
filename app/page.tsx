f586828b15f21df57488d86d32aa21ab7563d07b
jatin-vashisht-primathon <jatin.vashisht@primathon.in>
2025-02-28 15:36:31 +0530

    change for update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [updateAvailable, setUpdateAvailable] = useState(false);
+
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.addEventListener("controllerchange", () => {
+        setUpdateAvailable(true);
+      });
+    }
+  }, []);
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
   return (
-    <div className="h-screen w-screen bg-red-300">
-      Hello There
-    </div>
+    <>
+      <div className="h-screen w-screen bg-red-300">
+        Hello There Yp
+      </div> 
+      {updateAvailable && (
+        <div className="fixed bottom-4 left-4 right-4 bg-blue-500 text-white p-3 rounded-lg shadow-lg text-center">
+          A new version is available!{" "}
+          <button onClick={refreshPage} className="underline font-bold">
+            Update
+          </button>
+        </div>
+      )}
+    </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -15,5 +15,5 @@ export default withPWA({
     dest: "public",         // destination directory for the PWA files
     disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
     register: true,         // register the PWA service worker
-    skipWaiting: true,      // skip waiting for service worker activation
+    skipWaiting: false,      // skip waiting for service worker activation
 })(nextConfig);
